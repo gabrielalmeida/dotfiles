fd45bccf4e9da2c6777ea3e6367c99d5850c18a7
Gabriel Almeida <gabriel_almeida@me.com>
2023-09-15 14:36:32 -0300

    Update brew apps (wip)

--- scripts/steps/02_apps.sh
+++ scripts/steps/02_apps.sh
@@ -4,46 +4,46 @@ if [ "$FUNCTIONS_LOADED" != 'TRUE' ]; then
 fi
 
 install_brew_apps() {
-    brew install imagemagick --with-libtiff
-    brew install ffmpeg --with-libvpx --with-libvorbis --with-openssl --with-theora --with-x265
-    brew install --HEAD --with-bundle mpv
+    brew install imagemagick
+    brew install ffmpeg --with-libvorbis --with-openssl --with-theora --with-x265
+    brew install --HEAD mpv
 
-    brew install thefuck pandoc wakatime-cli z reattach-to-user-namespace subliminal cheat livestreamer aria2 asciinema atool ccat cpulimit duff duti exiftool eye-d3 gifify git git-extras ghi hub git-ftp haskell-stack hr httpie jq lftp mediainfo mkvtoolnix mp4v2 phantomjs pup shellcheck the_silver_searcher trash tree wiki youtube-dl fasd ranger watchman namebench mackup bat fd
+    brew install thefuck pandoc wakatime-cli z reattach-to-user-namespace subliminal cheat livestreamer aria2 asciinema atool ccat cpulimit duff duti exiftool eye-d3 gifify git git-extras ghi hub git-ftp haskell-stack hr httpie jq lftp mediainfo mkvtoolnix mp4v2 pup shellcheck the_silver_searcher trash tree wiki youtube-dl fasd ranger watchman namebench mackup bat fd
 
     brew tap railwaycat/emacsmacport
     brew install emacs-mac --with-spacemacs-icon --with-imagemagick
 }
 
 install_cask_apps() {
-  brew cask install calibre drop-to-gif dropbox flux fog gifloopcoder gitup
-  brew cask install google-chrome handbrakecli imageoptim jadengeller-helium key-codes screenflow
-  brew cask install shotcut spectacle transmission vlc mplayerx skype
-  brew cask install evernote sublime-text3 amethyst teamviewer spotify robomongo
-  brew cask install franz hyperterm mou launchrocket karabiner resolutionator
+  brew install --cask calibre drop-to-gif flux fog gifloopcoder gitup
+  brew install --cask google-chrome handbrakecli imageoptim jadengeller-helium key-codes screenflow
+  brew install --cask shotcut raycast transmission vlc mplayerx 
+  brew install --cask sublime-text3 amethyst teamviewer spotify robomongo
+  brew install --cask warp mou launchrocket karabiner resolutionator
 
   # install alternative versions
   brew tap caskroom/versions
-  brew cask install atom-beta google-chrome-canary
+  brew install --cask google-chrome-canary
 
   # prefpanes, qlplugins, colorpickers
-  brew cask install betterzipql colorpicker-skalacolor epubquicklook qlcolorcode qlimagesize qlplayground qlstephen quicklook-json ttscoff-mmd-quicklook qlmarkdown
+  brew install --cask betterzipql colorpicker-skalacolor epubquicklook qlcolorcode qlimagesize qlplayground qlstephen quicklook-json ttscoff-mmd-quicklook qlmarkdown
 
   # fonts
   brew tap caskroom/fonts
   # multiple
-  brew cask install font-alegreya font-alegreya-sans font-alegreya-sans-sc font-alegreya-sc
-  brew cask install font-source-code-pro font-source-sans-pro font-source-serif-pro
-  brew cask install font-pt-mono font-pt-sans font-pt-serif
-  brew cask install font-merriweather font-merriweather-sans
-  brew cask install font-fira-mono font-fira-sans
+  brew install --cask font-alegreya font-alegreya-sans font-alegreya-sans-sc font-alegreya-sc
+  brew install --cask font-source-code-pro font-source-sans-pro font-source-serif-pro
+  brew install --cask font-pt-mono font-pt-sans font-pt-serif
+  brew install --cask font-merriweather font-merriweather-sans
+  brew install --cask font-fira-mono font-fira-sans
   # sans
-  brew cask install font-exo2 font-lato font-open-sans font-open-sans-condensed font-signika
+  brew install --cask font-exo2 font-lato font-open-sans font-open-sans-condensed font-signika
   # serif
-  brew cask install font-abril-fatface font-butler font-gentium-book-basic font-playfair-display font-playfair-display-sc
+  brew install --cask font-abril-fatface font-butler font-gentium-book-basic font-playfair-display font-playfair-display-sc
   # slab
-  brew cask install font-bitter font-kreon
+  brew install --cask font-bitter font-kreon
   # script
-  brew cask install font-pacifico
+  brew install --cask font-pacifico
 }
 
 install_atom_packages() {
